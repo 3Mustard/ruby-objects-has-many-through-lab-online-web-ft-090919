b6d20cf9d9cac237547f965ffc671a18b25d7eae
Matthew C <m23.cullen@gmail.com>
2019-09-26 15:20:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,12 +12,14 @@ class Artist
   end
   
   def new_song(name,genre)
-    Song.new 
+    Song.new(name,self,genre) 
   end 
   
   def songs 
+    Song.all.select {|song| song.artist = self}
   end 
   
   def genres 
+    songs.map {|song| song.genre}
   end 
 end 
